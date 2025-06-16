c3b00b4222a1c0e481534f61ecb23d81620f91a3
rishi <rishirajsaha09@gmail.com>
2025-06-16 11:26:47 +0530

    Initial commit (cleaned history)

---
+++ 18-forkify/starter/src/js/model.js
@@ -0,0 +1,80 @@
+import { async } from 'regenerator-runtime';
+import { API_URL } from './config.js';
+import { getJSON } from './views/helpers.js';
+import { RES_PER_PAGE } from './config.js';
+
+
+export const state = {
+    recipe: {},
+    search: {
+        query: '',
+        results: [],
+        page: 1,
+        resultsPerPage: RES_PER_PAGE
+    }
+};
+
+export const loadRecipe = async function (id) {
+    try {
+        const data = await getJSON(`${API_URL}${id}`);
+        // 1 Loading recipe
+        // const res = await fetch('https://forkify-api.jonas.io/api/v2/recipes/664c8f193e7aa067e94e85be');
+
+
+        // console.log(res, data);
+        const { recipe } = data.data;
+        state.recipe = {
+            id: recipe.id,
+            title: recipe.title,
+            publisher: recipe.publisher,
+            sourceURL: recipe.source_url,
+            image: recipe.image_url,
+            servings: recipe.servings,
+            cookingTime: recipe.cooking_time,
+            ingredients: recipe.ingredients
+        }
+        console.log(state.recipe);
+    } catch (err) {
+        // Temporary error handling
+        console.error(`${err}`);
+        throw err;
+    }
+};
+
+export const loadSearchResults = async function (query) {
+    try {
+        state.search.query = query;
+        const data = await getJSON(`${API_URL}?search=${query}`);
+        console.log(data);
+
+        state.search.results = data.data.recipes.map(rec => {
+            return {
+                id: rec.id,
+                title: rec.title,
+                publisher: rec.publisher,
+                image: rec.image_url
+            };
+        });
+    } catch (err) {
+        console.error(`${err}`);
+        throw err;
+    }
+};
+
+export const getSearchResultsPage = function (page = state.search.page) {
+    state.search.page = page;
+
+    const start = (page - 1) * state.search.resultsPerPage;  // 0;
+    const end = page * state.search.resultsPerPage; // 9;
+
+
+    return state.search.results.slice(start, end);
+}
+export const updateServings = function (newServings) {
+    state.recipe.ingredients.forEach(ing => {
+        ing.quantity = (ing.quantity * newServings) / state.recipe.servings;
+        // new Quantity * new Servings / oldServings
+    });
+
+    state.recipe.servings = newServings;
+}

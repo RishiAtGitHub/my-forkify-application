185be4d3ebb569d044bef12180bfe1195e554c55
rishi <rishirajsaha09@gmail.com>
2025-06-18 09:02:37 +0530

    Saving changes

--- 18-forkify/starter/src/js/controller.js
+++ 18-forkify/starter/src/js/controller.js
@@ -22,14 +22,16 @@ const controlRecipes = async function () {
 
     // 0 Update results view to make selected search results
     resultsView.update(model.getSearchResultsPage());
+    // 1 Updating bookmarks view
     bookmarksView.update(model.state.bookmarks);
 
-    // 1 Loading recipe
+    // 2 Loading recipe
     await model.loadRecipe(id);
     // const { recipe } = model.state;
 
-    // 2 Rendering recipe
+    // 3 Rendering recipe
     recipeView.render(model.state.recipe);
+
   } catch (err) {
     recipeView.renderError();
   }
@@ -88,9 +90,14 @@ const controlAddBookmark = function () {
 
   // 3 Render bookmarks
   bookmarksView.render(model.state.bookmarks);
-}
+};
+
+const controlBookmarks = function () {
+  bookmarksView.render(model.state.bookmarks);
+};
 
 const init = function () {
+  bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
   recipeView.addHandlerUpdateServings(controlServings);
   recipeView.addHandlerAddBookmark(controlAddBookmark);
@@ -99,3 +106,4 @@ const init = function () {
 };
 
 init();
+
